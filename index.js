eed8d0c3561acc493550cb0b6091ad1cef92ac0c
csrapr <a79014@alunos.uminho.pt>
2020-09-23 20:01:01 +0100

    authentication and mongoose

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const express = require("express");
-const passport = require("passport");
 const keys = require("./config/keys");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh Token", refreshToken);
-      console.log("profile", profile);
-    }
-  )
+app.use(
+  cookieSession({
+    maxAge: 1000 * 60 * 60 * 24, //1 day in ms
+    keys: [keys.cookieKey],
+  })
 );
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,43 @@
+const passport = require("passport");
+const keys = require("../config/keys");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const userFromDB = await User.findOne({ googleId: profile.id });
+        if (userFromDB) {
+          done(null, userFromDB);
+        } else {
+          //new user!
+          const newUser = await new User({ googleId: profile.id }).save();
+          done(null, newUser);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  )
+);
