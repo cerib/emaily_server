4f60b38335d6b951b5db3d53f93374a9df38082c
csrapr <a79014@alunos.uminho.pt>
2020-09-24 12:03:22 +0100

    heroku configs

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("emaily_client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "emaily_client", "build", "index.html")
+    );
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log("Listening on port " + PORT);
